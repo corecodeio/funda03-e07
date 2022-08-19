8641307bcd3f044dbe5694e1896aba6bcafeda85
Yosef <yosefmarr@gmail.com>
2022-08-18 19:10:18 -0600

    first commit

---
+++ typescript/Example.js
@@ -0,0 +1,7 @@
+console.log('Hola');
+function sum(a, b) {
+  return a + b;
+}
+console.log(sum(4, 10));
+console.log(sum(50, 10));
+console.log(sum(90, 10));
---
+++ typescript/Example.ts
@@ -0,0 +1,9 @@
+console.log('Hola');
+
+function sum(a: number, b: number) {
+  return a + b;
+}
+
+console.log(sum(4, 10));
+console.log(sum(50, 10));
+console.log(sum(90, 10));
